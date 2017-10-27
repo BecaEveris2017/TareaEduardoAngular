cdceec194103985e069f40efeecf17f6f288514a
Eduardo Almaraz Pintor <won_mine@hotmail.com>
2017-10-27 18:16:27 -0500

    Tarea 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre:string;
+  colores:string[];
+  placeholder:string;
+
+  constructor() {
+    this.nombre = 'Eduardo Almaraz Pintor';
+    this.colores = ['Amarillo', 'Verde', 'Azul'];
+    this.placeholder = 'Ingresa un color';
+  }
+
+  addColor(color) {
+    this.colores.push(color.value);
+    console.log(color.value);
+    color.value = '';
+    this.placeholder = 'Ingresa otro color';
+    return false;
+  }
 }
